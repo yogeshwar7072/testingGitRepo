02c54705354ca73694dff54114fa19443813992c
yogeshwar chate <yogeshwarchate15@gmail.com>
2023-01-02 10:46:06 +0530

    Eerror.java

--- src/main/java/com/first/org/Log4J/Eerror.java
+++ src/main/java/com/first/org/Log4J/Eerror.java
@@ -5,6 +5,18 @@ public class Eerror {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
+		for (int i = 0; i < 10; i++) {
+			System.out.println("hello");
+		}
+		for (int i = 0; i < 10; i++) {
+			System.out.println("hello");
+		}
+		for (int i = 0; i < 10; i++) {
+			System.out.println("hello");
+		}
+		for (int i = 0; i < 10; i++) {
+			System.out.println("hello");
+		}
 	}
 
 }
